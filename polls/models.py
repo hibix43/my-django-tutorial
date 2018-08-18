5a200c0c0bdd6868d59c92ea89f8a7ccb504ba47
HibikiYoshida <b1600661@planet.kanazawa-it.ac.jp>
2018-08-18 23:26:43 +0900

    add Question and Choice Model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    # 質問事項（テキストデータ）
+    question_text = models.CharField(max_length=200)
+    # 公開日（日時データ）
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    # ChoiseをQuestionに関連づける
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # 選択肢
+    choice_text = models.CharField(max_length=200)
+    # 投票数（整数データ）
+    votes = models.IntegerField(default=0)
+
