a623ed8ac827088641ebd283de8c7f09e89c0f08
HibikiYoshida <b1600661@planet.kanazawa-it.ac.jp>
2018-08-20 18:23:39 +0900

    add app_name and finish tutorial3

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # ex /polls/
     path('', views.index, name='index'),
-    # ex /polls/1
+    # ex /polls/1 name引数指定により{% url detail (引数)が可能 %}
     path('<int:question_id>/', views.detail, name='detail'),
     # ex /polls/1/results/
     path('<int:question_id>/results/', views.results, name='results'),
