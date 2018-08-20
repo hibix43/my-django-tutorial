da55d0962cbac936dce8432ef50a7ea9ba4baa12
HibikiYoshida <b1600661@planet.kanazawa-it.ac.jp>
2018-08-20 23:07:32 +0900

    add vote-form and results.html.

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.urls import reverse
+from .models import Choice, Question
 
 def index(request):
     # 最新５件を取得
@@ -10,12 +11,26 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
+    # try-exceptせずに404
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at question %s." 
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You',re voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # エラーメッセージを引数に表示し直し
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # 成功したら結果ページへ。reverse()によりpoll/id/resultsを取得
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
