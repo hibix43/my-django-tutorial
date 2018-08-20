4c865f8a56232c8f06c45aeab9e194487bb2db2e
HibikiYoshida <b1600661@planet.kanazawa-it.ac.jp>
2018-08-20 18:15:00 +0900

    change Using template system instead of hardcoding.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 def index(request):
@@ -10,12 +10,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    # 例外処理にて404Error処理
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question_id})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at question %s." 
