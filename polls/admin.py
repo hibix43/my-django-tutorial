0ff94fdf260bd298dca18bab7842ebadd9f02673
HibikiYoshida <b1600661@planet.kanazawa-it.ac.jp>
2018-08-22 22:42:08 +0900

    add custom admin form.

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    # Adim編集フォームの並び順を指定
+    # フォームをまとめて見出しとともに表示
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']})
+    ]
+
+# Modelごとに引数で渡す
+admin.site.register(Question, QuestionAdmin)
