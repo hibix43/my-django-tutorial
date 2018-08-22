87e8e370e1c06ded4f48aec93cccb2b2ca13c04c
HibikiYoshida <b1600661@planet.kanazawa-it.ac.jp>
2018-08-22 23:16:01 +0900

    add New Choice slot (Stack).

--- polls/admin.py
+++ polls/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
 from .models import Question, Choice
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    # 余分に表示
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     # Adim編集フォームの並び順を指定
-    # フォームをまとめて見出しとともに表示
+    # fields:フォームをまとめて見出しとともに表示
+    # classes: collapseにて折りたたみ表示にする
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']})
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']})
     ]
+    # XXInlineによりXX表示(ex:Stack>>列表示)
+    inlines = [ChoiceInline]
 
 # Modelごとに引数で渡す
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
